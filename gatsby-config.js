da12a8dda00d2858ea1b242a397d781069fa3aa6
kc-nxtbig <kc@nxtbig.com>
2022-09-08 09:24:31 +0530

    remove plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
+    /*  {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -29,6 +29,6 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/android-chrome-512x512.png`, // This path is relative to the root of the site.
       },
-    },
+    }, */
   ],
 }
